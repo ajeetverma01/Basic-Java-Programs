1309be0ee36eb9bb049efcf2716b6d6ade385aa4
Ajeet Verma <av401402@gmail.com>
2025-03-03 17:58:26 +0530

    Initial commit for Palindrome3

--- src/Pallindrome_Finder/Palindrome2.java
+++ src/Pallindrome_Finder/Palindrome2.java
@@ -1,4 +1,30 @@
 package Pallindrome_Finder;
 
+import java.util.Scanner;
+
 public class Palindrome2 {
+    public static void isPalindrome(String str)
+    {
+        String rev = "";
+        boolean ans = false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        if (ans)
+            System.out.println("It is a Palindrome word.");
+        else
+            System.out.println("It is not a Palindrome word.");
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String str =sc.nextLine();
+
+        str = str.toLowerCase();
+        isPalindrome(str);
+    }
 }
