efdacc4902176674f70d103ca29469642f3d1c1c
Ajeet Verma <av401402@gmail.com>
2025-03-03 17:59:41 +0530

    Palindrome2, 3 completed

--- src/Pallindrome_Finder/Palindrome3.java
+++ src/Pallindrome_Finder/Palindrome3.java
@@ -1,4 +1,19 @@
 package Pallindrome_Finder;
+import java.util.Scanner;
 
 public class Palindrome3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String str =sc.nextLine();
+
+        StringBuilder str1 = new StringBuilder(str);
+        str1.reverse();
+
+        if (str.equals(str1.toString())) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a palindrome String");
+        }
+    }
 }
