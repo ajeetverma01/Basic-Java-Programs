7f386702182c58e7f6ed4dda40e6cd6bb8d9fef3
Ajeet Verma <av401402@gmail.com>
2025-03-11 08:13:32 +0530

    Completed StringOperations class

--- src/StringOperations.java
+++ src/StringOperations.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class StringOperations {
+import java.util.Scanner;
+class StringOperations {
+    public static void main(String[] args) {
+        String xx = new String();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter input string: ");
+        xx = sc.nextLine();
+        System.out.println("Uppercase:  " + xx.toUpperCase());
+        System.out.println("Lowercase: " + xx.toLowerCase());
+        System.out.println("Is empty: " + xx.isEmpty());
+        System.out.println("Starts with  " + xx.endsWith(xx));
+    }
 }
