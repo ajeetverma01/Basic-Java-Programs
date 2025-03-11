4fdff0f967f3bdbacc394818d8ba253c7e8336d0
Ajeet Verma <av401402@gmail.com>
2025-03-11 07:51:14 +0530

    GetReverse Completed

--- src/GetReverse.java
+++ src/GetReverse.java
@@ -1,2 +1,14 @@
-public class GetReverse {
+class GetReverse
+{
+    public static void main(String[] a)
+    {
+        int number=321;
+        int reverse=0;
+        while(number>0)
+        {
+            reverse=(reverse*10)+(number%10);
+            number=number/10;
+        }
+        System.out.println(reverse);
+    }
 }
