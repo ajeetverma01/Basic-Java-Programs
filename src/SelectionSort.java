827532353f2d4826e7b23c138f2e5a3866115a01
Ajeet Verma <av401402@gmail.com>
2025-03-11 08:09:50 +0530

    Selection Sort Completed

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,23 @@
-public class SelectionSort {
+class SelectionSort
+{
+    public static void main(String[] args)
+    {
+        int i,j,flag;
+        int rawData[]={0,1,2,3,4,5,6,7,8,9};
+        for(i=0;i<rawData.length;i++)
+        {
+            for(j=0;j<rawData.length;j++)
+            {
+                if(rawData[i]<rawData[j])
+                {
+                    flag=rawData[i];
+                    rawData[i]=rawData[j];
+                    rawData[j]=flag;
+                }
+            }
+        }
+        System.out.println("Sorted...");
+        for(i=0;i<rawData.length;i++)
+            System.out.print(rawData[i]+" ");
+    }
 }
